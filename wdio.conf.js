c03e4ab8bc99a7757c949d5a65d92cd1ae1c0ba4
Suryaprasath <suryajack27@gmail.com>
2025-04-29 23:45:15 +0530

    my commit-1

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,6 +51,8 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
+    services: ['chromedriver'],
+
     capabilities: [{
         browserName: 'chrome'
     }],
